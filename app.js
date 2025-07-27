e3d2963384a382e8e7d4eaa111ff586541823f59
chinomedel <nmedelb@gmail.com>
2025-07-27 19:31:41 -0400

    Valida input

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+/* Variables globales */
+var listaAmigos = [];
+
+function agregarAmigo() {
+
+    let nombre = document.getElementById('amigo').value; // Obtiene el valor del input con id 'nombre'
+    console.log(nombre); // Imprime el nombre en la consola para depuración
+    if(nombre == ''){
+        alert('El campo no puede estar vacío'); // Muestra un mensaje de alerta si el campo está vacío
+        return false; // Detiene la ejecución si el campo está vacío
+    }else{
+        listaAmigos.push(nombre.trim());
+        return true; // Indica que se agregó correctamente
+    }
+}
